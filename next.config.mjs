f91b9956f0697b0eef16201eea51f4562c64d41a
Mark Tarzan Kaggwe <124345183+kaggwetarzanmark@users.noreply.github.com>
2025-12-09 23:57:43 +0300

    Deploy static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
-}
+  basePath: '/dianaevents',   // <-- your repo name
+  assetPrefix: '/dianaevents/', 
+};
 
-export default nextConfig
+export default nextConfig;
