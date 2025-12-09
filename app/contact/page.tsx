de62635a2be8b5d824921a48f43eaf4774893f2f
Mark Tarzan Kaggwe <124345183+kaggwetarzanmark@users.noreply.github.com>
2025-12-10 00:26:10 +0300

    Deploy static site

--- app/contact/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ContactPage } from "@/components/pages/contact-page"
-
-export default function Contact() {
-  return <ContactPage />
-}
--- app/services/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ServicesPage } from "@/components/pages/services-page"
-
-export default function Services() {
-  return <ServicesPage />
-}
